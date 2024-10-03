a9c43cd7019b2e66fdfb997cfc552a5e1f2ab278
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2024-10-03 16:55:45 -0600

    refactor: Remove previous solution that cause conflicts with middleware logic

--- src/middleware/apply-middleware.ts
+++ src/middleware/apply-middleware.ts
@@ -1,9 +1,20 @@
 import type { Handler, Middleware } from "@/types.ts";
 
 export function applyMiddleware(...middlewares: Middleware[]) {
-  return (handler: Handler) => {
-    return middlewares.reduceRight((_, middleware) => {
-      return (request) => Promise.resolve(middleware(request, handler));
-    }, handler);
+  if (middlewares.length === 0) {
+    return (next: Handler) => next;
+  }
+
+  return (next: Handler) => {
+    let handler = next;
+
+    for (const middleware of middlewares.reverse()) {
+      const prevHandler = handler;
+
+      handler = (request: Request) =>
+        Promise.resolve(middleware(request, prevHandler));
+    }
+
+    return handler;
   };
 }
