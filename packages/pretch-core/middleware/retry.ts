3810daeb0e570897e846854894de29efac09caa7
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2024-10-25 17:57:39 -0600

    refactor(core): Removed useless "retry" function

--- packages/pretch-core/middleware/retry.ts
+++ packages/pretch-core/middleware/retry.ts
@@ -40,22 +40,3 @@ export function retryMiddleware(
     }
   };
 }
-
-export function retry(count: number): Middleware {
-  return async (request, next) => {
-    let index = 0;
-
-    while (true) {
-      index++;
-      try {
-        return await next(request.clone());
-      } catch (error) {
-        if (index < count) {
-          continue;
-        } else {
-          throw error;
-        }
-      }
-    }
-  };
-}
