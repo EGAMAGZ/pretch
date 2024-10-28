f666463474fde92966feaa0c3ded9fabaebd1df6
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2024-10-28 17:21:37 -0600

    test(core): Improved tests

--- packages/pretch-core/build-fetch.ts
+++ packages/pretch-core/build-fetch.ts
@@ -1,7 +1,12 @@
 import type { CustomFetch, Enhancer, Handler } from "@/types.ts";
 
 /**
- * Creates a custom fetch function with optional middleware enhancement.
+ * Creates a custom fetch function with optional enhancement.
+ *
+ * @example Create a custom fetch without any enhancement
+ * ```ts
+ * import { buildFetch } from "@pretch/core";
+ * ```
  *
  * @param {Enhancer} [enhancer] - An optional function to enhance the fetch behavior.
  * @returns {CustomFetch} A custom fetch function that applies the enhancer, if provided.
