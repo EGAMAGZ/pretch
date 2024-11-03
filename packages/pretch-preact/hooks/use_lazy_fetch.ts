4a36afa46cc375f8f6582d6a8bbe36c180231dcc
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2024-11-03 17:59:37 -0600

    doc(preact): Wrote JSDoc for hooks and the corresponding types

--- packages/pretch-preact/hooks/use_lazy_fetch.ts
+++ packages/pretch-preact/hooks/use_lazy_fetch.ts
@@ -1,8 +1,17 @@
-import type { Enhancer } from "@pretch/core";
+import { type Enhancer, buildFetch } from "@pretch/core";
 import { useSignal } from "@preact/signals";
-import { buildFetch } from "../../pretch-core/build_fetch.ts";
 import type { LazyFetchResult } from "@/types.ts";
 
+/**
+ * A hook that creates a custom fetch function with optional enhancement
+ * and tracks the status of the request. Fetches the data manually.
+ *
+ * @param {string} url - The URL to fetch.
+ * @param {RequestInit} [options] - The options for the request.
+ * @param {Enhancer} [enhancer] - An optional function to enhance the fetch behavior.
+ * @returns {LazyFetchResult<T>} An object with the `data`, `loading`, and `error` properties
+ *   and a `fetchData` method.
+ */
 export function useLazyFetch<T>(
   url: string,
   options?: RequestInit,
--- packages/pretch-preact/types.ts
+++ packages/pretch-preact/types.ts
@@ -1,3 +1,12 @@
+/**
+ * Represents the result of a fetch operation.
+ *
+ * @template T The type of the data returned by the fetch.
+ * @property {T | null} data - The data returned by the fetch, or null if not available.
+ * @property {boolean} loading - Indicates whether the fetch operation is in progress.
+ * @property {Error | null} error - The error encountered during the fetch, or null if none.
+ * @property {(newUrl?: string) => void} refetch - A function to refetch the data with an optional new URL.
+ */
 export type FetchResult<T> = {
   data: T | null;
   loading: boolean;
@@ -5,6 +14,15 @@ export type FetchResult<T> = {
   refetch: (newUrl?: string) => void;
 };
 
+/**
+ * Represents the result of a lazy fetch operation, where fetching is triggered manually.
+ *
+ * @template T The type of the data returned by the fetch.
+ * @property {T | null} data - The data returned by the fetch, or null if not available.
+ * @property {boolean} loading - Indicates whether the fetch operation is in progress.
+ * @property {Error | null} error - The error encountered during the fetch, or null if none.
+ * @property {(newUrl?: string, newOptions?: RequestInit) => void} fetchData - A function to fetch the data manually with optional new URL and request options.
+ */
 export type LazyFetchResult<T> = {
   data: T | null;
   loading: boolean;
