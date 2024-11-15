fc2cee640ceeceb484f63838c8cc5414ffffe30e
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2024-11-14 19:04:26 -0600

    fix(preact,react): Exported missing hooks

--- packages/pretch-react/mod.ts
+++ packages/pretch-react/mod.ts
@@ -3,4 +3,5 @@
  * Pretch implementation for react by hooks
  */
 export { useFetch } from "@/hooks/use_fetch.ts";
+export { useLazyFetch } from "@/hooks/use_lazy_fetch.ts";
 export * from "@/types.ts";
